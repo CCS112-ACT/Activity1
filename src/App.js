fe15b8b1c100bfc96dd4547f446701985afaeb29
Solis-KimAlthea <soliskim36@gmail.com>
2024-04-06 09:13:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ const products = [
   { id: 3, name: 'Shower Gel', description: 'Invigorate your senses with our luxurious shower gel', price: 20 },
   { id: 4, name: 'Conditioner', description: 'Achieve silky smooth hair with our revitalizing conditioner', price: 13 },
   { id: 5, name: 'Face Wash', description: 'Gently cleanse and rejuvenate your skin with our refreshing face wash', price: 50 }, 
-]
+  { id: 6, name: 'Shaving Cream', description: 'Experience a smooth and comfortable shave with our rich shaving cream', price: 70 },
+  { id: 7, name: 'Lotion', description: 'Hydrate and soften your skin with our moisturizing lotion', price: 85 },
+  { id: 8, name: 'Deodorant', description: 'Stay fresh and confident all day with our long-lasting deodorant', price: 75 },
+  { id: 9, name: 'Body Scrub', description: 'Exfoliate and renew your skins radiance with our invigorating body scrub', price: 90 },
+  { id: 10, name: 'Hand Sanitizer', description: 'Keep your hands clean and protected on-the-go with our convenient hand sanitizer', price: 95 },
+];
 
 const Product = ({ product, onAddToCart }) => {
   const { name, description, price } = product;
